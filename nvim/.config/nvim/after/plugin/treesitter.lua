ba602cc139ea8c0bf15304f2a01a3aac1bd49093
hndrk <51416554+hendriknielaender@users.noreply.github.com>
2023-04-07 23:55:21 +0200

    fix: change ensure_installed help to vimdoc

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "help", "javascript", "typescript", "go", "lua", "rust" },
+    ensure_installed = { "vimdoc", "javascript", "typescript", "go", "lua", "rust", "toml" },
   
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
