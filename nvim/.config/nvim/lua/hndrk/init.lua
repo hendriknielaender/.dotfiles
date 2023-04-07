93b1b2b6939b483da212ed0d8bc5f3c7ec957e6f
Hendrik Nieländer <hendrik@nielaender.de>
2023-04-07 17:27:57 +0200

    fix: vim mapleader key

--- nvim/.config/nvim/lua/hndrk/init.lua
+++ nvim/.config/nvim/lua/hndrk/init.lua
@@ -36,6 +36,8 @@ autocmd({"BufWritePre"}, {
     command = "%s/\\s\\+$//e",
 })
 
+vim.g.mapleader = " "
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- nvim/.config/nvim/lua/hndrk/remap.lua
+++ nvim/.config/nvim/lua/hndrk/remap.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
