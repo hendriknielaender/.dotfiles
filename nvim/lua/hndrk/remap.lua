c9965918f3ddf21f7d630937607b2632904b6df2
Hendrik Nieländer <hendrik@nielaender.de>
2024-02-05 21:21:36 +0100

    chore: add nvim keymaps

--- nvim/lua/hndrk/remap.lua
+++ nvim/lua/hndrk/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -35,9 +35,18 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
 -- Git
 vim.keymap.set("n", "<leader>gc", ":Git commit -m \"")
 vim.keymap.set("n", "<leader>gp", ":Git push -u origin HEAD<CR>")
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
