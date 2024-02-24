7ac88f9b292b8076670bd458993ea08e23733c32
Hendrik Nieländer <hendrik@nielaender.de>
2024-02-24 21:44:48 +0100

    chore: add vim keymaps

--- nvim/lua/hndrk/remap.lua
+++ nvim/lua/hndrk/remap.lua
@@ -2,6 +2,12 @@ vim.keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
